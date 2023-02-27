21e029d34f7bbf57c1c2dfa063821c2dabf5ae09
umjiii <sarel087@protonmail.com>
2023-02-27 12:08:07 -0700

    Fixed spacing. Program now complete, but could use a win message.

--- DiceGame.c
+++ DiceGame.c
@@ -1,3 +1,14 @@
+/*
+Author: Stephen Arel
+Class: CSC 240
+Assignment: DiceGame
+Description:   Turns are taken using the playerTurn() method, and two dice are rolled. If snake-eyes (1 & 1) are rolled,
+            then add 25 points to that player. If they roll doubles otherwise, then add points equal to twice the number of the two
+            dice rolls summed. Otherwise, just add the points equal to the sum of the two rolls to the current player's point total.
+            If they roll only a single one, then their turn ends, their score is turned to 0, and it is the next player's turn.
+            Keep switching turns between players, and calculating points until one of the players reaches a score of at least 100. Then,
+            then game ends.
+*/
 #include <stdio.h>
 #include <stdlib.h> //NULL constant, srand(), & rand() functions
 #include <time.h> //access your computer's clock time
@@ -8,6 +19,8 @@ static int diceTwo;
 static int p1Points;
 static int p2Points;
 static char again;
+int turnCount = 0;
+int one = 0;
 
 static int playerNum;
 struct die;
@@ -33,10 +46,7 @@ int main(){
 
  playerTurn();
 
- printf("Player 1 score: %d\nPlayer 2 score: %d", *pPlay1, *pPlay2);
-
- return 0;
-
+ return 0; //terminate process
 }
 
 
@@ -45,25 +55,37 @@ int main(){
 //player turn
 void playerTurn()
 {
-    printf("Player 1 score: %d\nPlayer 2 score: %d\n+========================================+\n", *pPlay1, *pPlay2);
-        do {
+    one = 0; //is
+    if(turnCount == 0) //if this is the first turn. if it is, don't add any new lines (spaces).
+    {
+        printf("Player 1 score: %d\nPlayer 2 score: %d\n+========================================+\n", *pPlay1, *pPlay2);
+    }
+    else //not the first turn, add a space between lines.
+    {
+        printf("\nPlayer 1 score: %d\nPlayer 2 score: %d\n+========================================+\n", *pPlay1,
+               *pPlay2);
+    }
+        do { //do/while loop to roll the dice until the user decides to end the turn (again != 'Y' || 'y')
             rollDice();
-            if (*pdo != *pdt && (*pdo == 1 || *pdt ==1)) {
-                *pPlay1 = 0;
-                again = 'n';
+            if (*pdo != *pdt && (*pdo == 1 || *pdt ==1)) { //is only one of the dice are equal to 1
+                *pPlay1 = 0; //score is now equal to 0
+                again = 'n'; //don't roll again
+
             }
             else
-                *pPlay1 += calcPoints(pdo, pdt);
+                *pPlay1 += calcPoints(pdo, pdt); //if only one of dice are not 1, then calculate points based on dice.
 
-            printf("Player 1 rolls %d and %d. Your total for this turn is %d.",*pdo, *pdt, *pPlay1);
+            printf("Player 1 rolls %d and %d. Your total for this turn is %d.",*pdo, *pdt, *pPlay1); //display the dice rolls and points for player 1.
 
-            if (*pPlay1 != 0)
+            if (*pPlay1 != 0) //if the score is not 0 (if only one of the dice are not 1)
             {
                 printf("\n\tDo you want to roll again (Y/N)?");
                 scanf(" %c", &again);
             }
 
-            printf("\r\n");
+            printf("\r\n"); //new lines to improve readability
+
+            //player 1 do/while is identical to player 2 with minor changes.
 
         } while (again == 'Y' | again =='y');
 
@@ -72,11 +94,17 @@ void playerTurn()
             if (*pdo != *pdt && (*pdo == 1 || *pdt ==1)) {
                 *pPlay2 = 0;
                 again = 'n';
+                one = 1; //if the turn for player 2 is over, return a binary boolean 1.
             }
             else
                 *pPlay2 += calcPoints(pdo, pdt);
 
-            printf("Player 2 rolls %d and %d. Your total for this turn is %d.",*pdo, *pdt, *pPlay2);
+            if(one == 1) //if the turn for player 2 is over, then add a space after the score and rolls are displayed.
+            {
+                printf("\nPlayer 2 rolls %d and %d. Your total for this turn is %d.\n",*pdo, *pdt, *pPlay2);
+            }
+            else //if not, then don't add a space but still display the rolls and turn score.
+               printf("\nPlayer 2 rolls %d and %d. Your total for this turn is %d.",*pdo, *pdt, *pPlay2);
 
             if (*pPlay2 != 0)
             {
@@ -84,11 +112,11 @@ void playerTurn()
                 scanf(" %c", &again);
             }
 
-            printf("\r\n");
         } while (again == 'Y' | again =='y');
 
-        if (*pPlay1 < 100 || *pPlay2 < 100)
+        if (*pPlay1 < 100 || *pPlay2 < 100) //if one of the players have not reached a score of at least 100, then keep taking turns (recursively)
         {
+            turnCount++; //return the count of turns for the purpose of deciding whether it is the first turn or not.
             playerTurn();
         }
 }
@@ -97,13 +125,13 @@ void playerTurn()
 int calcPoints(int* pdo, int* pdt)
 {
     if (*pdo == *pdt && *pdo == 1) //if double-ones
-        return 25;
+        return 25; //add 25 points
 
-    else if(*pdo == *pdt)
+    else if(*pdo == *pdt) //if rolling doubles but not ones, then return double the number rolled between the two dice
         return 2 * (*pdt + *pdo);
 
     else
-        return *pdo + *pdt;
+        return *pdo + *pdt; //return the points as the sum of the numbers rolled between each die
 }
 
 //rollDice ------ pointers passed to assign random numbers to the value of their addresses
